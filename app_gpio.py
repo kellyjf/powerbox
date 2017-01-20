a0907f37e1a3dc3deb60946a9ea71d9881fa176d
John Kelly <jkelly@aircell.com>
2017-01-19 22:56:00 -0700

    Expanding buttons

--- app_gpio.py
+++ app_gpio.py
@@ -5,20 +5,57 @@ from PyQt4.QtGui import *
 from ui_gpio import *
 from app_monitor import *
 
+import os
+
+topdir="/sys/class/gpio"
+topdir="/tmp/class/gpio"
+
+class OutletButton(QPushButton):
+        def __init__(self, number):
+                super(QPushButton,self).__init__()
+                self.number = number
+                self.setCheckable(True)
+                self.setText("Outlet %d"%(number))
+                self.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
+                self.connect(self, SIGNAL("toggled(bool)"), self.toggled)
+		self.toggled(False)
+
+        def toggled(self, state):
+		print self.number, state
+                f = open("%s/gpio%d/value"%(topdir,496+self.number),"w")
+                f.write("%d\n"%(0 if state else 1))
+                f.close()
+		self.setText("Outlet %d %s"%(self.number,"OFF" if state else "ON"))
+
+
+	
 class GpioDialog(Ui_GpioDialog, QDialog):
 
 	def __init__(self, parent=None):
 		super(QDialog,self).__init__(parent)
 		self.setupUi(self)
 		self.setWindowFlags(Qt.FramelessWindowHint)
-		self.pixmap=QPixmap("trump-buttons.png")
-		self.imageLabel.setPixmap(self.pixmap)
 		self.wThread=[]
 		self.wMonitor=[]
-		self.labels=[self.num1Label,self.num2Label, self.num3Label]
-		self.buttons=[self.num1Button,self.num2Button, self.num3Button]
 
-		for num,gpio in enumerate([ 18, 23, 24]):
+		for i in range(8):
+			gpiodir = "%s/gpio%d"%(topdir,496+i)
+			if not os.path.isdir(gpiodir):
+				f = open("%s/export"%(topdir),"w")
+				f.write("%d\n"%(496+1))
+				f.close()
+				f = open("%s/direction"%(gpiodir),"w")
+				f.write("out\n")
+				f.close()
+				f = open("%s/value"%(gpiodir),"w")
+				f.write("1\n")
+				f.close()
+
+			button = OutletButton(i)
+			self.powerGrid.addWidget(button,i%4,i/4)
+
+			
+		for num,gpio in enumerate([]):
 			print "num %d gpio %d"%(num,gpio)
 			self.wThread.append(QThread())
 			self.wMonitor.append(GpioMonitor(gpio))
